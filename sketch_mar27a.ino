a039442092a6e693f22d6a83daf9c1b83c5ef6a3
SimplyPotato <167816575+lewisc65@users.noreply.github.com>
2026-03-28 09:45:43 -0500

    Add files via upload

--- sketch_mar27a.ino
+++ sketch_mar27a.ino
@@ -1,5 +1,8 @@
+#include <math.h>
 long duration;
-int distance;
+float distance;
+float oldDistance;
+int freq;
 
 const int trigPin = 10;
 const int echoPin = 11;
@@ -29,8 +32,42 @@ void loop() {
   //Conversion into centimeters
   distance = duration * 0.034 /2;
 
+  //Clean data
+  // if((distance - oldDistance) > 10){
+  //   distance = oldDistance;
+  // }
+
+  //Frequency 
+  //freq = 440*pow(2, (distance/12));
+
+  // freq = (distance * 20) + 200;
+  
+  //Scale programmed
+  if(distance < 4){
+    freq = 2093;
+  } else if(distance < 7){
+    freq = 2349.32;
+  } else if(distance < 10){
+    freq = 2637.02;
+  } else if(distance < 13){
+    freq = 2793.83;
+  } else if(distance < 16){
+    freq = 3135.96;
+  } else if(distance < 19){
+    freq = 3520;
+  } else if(distance < 22){
+    freq = 3951.07;
+  } else if(distance < 38){
+    freq = 4186.01;
+  } else{
+    freq = 0;
+  }
+  tone(8, freq);
+  oldDistance = distance;
+
   //Print distance
   Serial.print("Distance: ");
   Serial.println(distance);
-  delay(100);
+  Serial.println(freq);
+  delay(3);
 }
